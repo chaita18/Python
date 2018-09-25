f7e604cfd8ec5048fd7a20edb5ae647a8e9dfff7
chaita18 <chaitalimehta91@gmail.com>
2018-09-25 17:25:12 +0530

    Commiting greet user and addition of two numbers programs

---
+++ Programms/greet_user.py
@@ -0,0 +1,3 @@
+#!C:/Users/R4J/AppData/Local/Programs/Python/Python37-32
+user_name = eval(input("Enter your name ('Ex: \"Chaitali\"') : "))
+print("Hello",user_name)
---
+++ Programms/two_num_addition.py
@@ -0,0 +1,4 @@
+#!C:/Users/R4J/AppData/Local/Programs/Python/Python37-32
+num1 = eval(input("Enter first number : "))
+num2 = eval(input("Enter second number : "))
+print("Addition of two numbers : ",num1+num2)
